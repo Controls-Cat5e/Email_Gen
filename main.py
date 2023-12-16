7a6d8bab9914222d30c7304cf42927f05d467834
Abhiram <gamebusterfive@gmail.com>
2023-12-15 22:01:15 -0800

    test Gemini

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import pprint
-import google.generativeai as palm
-st.set_page_config(page_title="Prototype-TextGen")
-palm.configure(api_key=st.secrets["api_key"])
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
-model = models[0].name
+import google.generativeai as gemini
+st.set_page_config(page_title="Email Gen-V1.0", page_icon=":email:",initial_sidebar_state="expanded")
+gemini.configure(api_key=st.secrets["api_key"])
+models = [m for m in gemini.list_models() if 'generateText' in m.supported_generation_methods]
+model = gemini.GenerativeModel('gemini-pro')
 
 
 temp = 0.7
 outputSize = 1000
 
-def PaLMgen(prompt, temp, outputSize):
-    completion = palm.generate_text(
+def geminiGen(prompt, temp, outputSize):
+    completion = gemini.generate_content(
         model=model,
         prompt=prompt,
         temperature=temp,
@@ -21,8 +21,7 @@ def PaLMgen(prompt, temp, outputSize):
     return completion.result
 
 
-
-st.title("Hello , Welcome to Text Generator!")
+st.title("Hello , Welcome to Email Generator V1.0!")
 option = st.radio("Please select the type of your text", ("Email", "Message"))
 content = st.text_area("Please type the content of your email here", height=100)
 tone = st.selectbox("Please select the tone of your email", ("Professional email short","Formal", "Informal", "Casual", "Business", "Informative", "question", "neutral", "other"))
@@ -45,7 +44,7 @@ if st.button("Generate Email"):
                 {content}
                 you will perform grammar refinements, fix typos and follow the tone above
                 """
-                st.write(PaLMgen(prompt, creativity, outputSize))
+                st.write(geminiGen(prompt, creativity, outputSize))
                 
             else:
                 prompt = f"""
@@ -56,7 +55,7 @@ if st.button("Generate Email"):
                 {content}
                 you will perform grammar refinements, fix typos and follow the tone above:
                 """
-                st.write(PaLMgen(prompt, creativity, outputSize))
+                st.write(geminigen(prompt, creativity, outputSize))
                 
             
         st.success('Your email has been generated! 	:star2:')
@@ -72,7 +71,7 @@ if st.button("Generate Email"):
                 {content}
                 you will perform grammar refinements, fix typos and follow the tone above
                 """
-                st.write(PaLMgen(prompt, creativity, outputSize))
+                st.write(geminigen(prompt, creativity, outputSize))
                 
             else:
                 prompt = f"""
@@ -83,7 +82,7 @@ if st.button("Generate Email"):
                 {content}
                 you will perform grammar refinements, fix typos and follow the tone above
                 """
-                st.write(PaLMgen(prompt, creativity, outputSize))
+                st.write(geminigen(prompt, creativity, outputSize))
                 
             
         st.success('Your message has been generated! 	:star2:')
